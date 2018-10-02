b6703b9f9f28b3ae34bd1d916ddacca3f0e5fbc0
xsjsea <13914129546@139.com>
2018-10-02 10:40:20 +0800

    for ignore

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,31 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from 'chat_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    # ActionCable.server.broadcast('messages',
+    #   message: render_message(data['message']))
+    ActionCable.server.broadcast 'room_channel', message: data['message']
+    Message.create! content: data['message'], order_id: data['order_id'], step_order: data['step_order'], user_id: data['user_id']
+  end
+
+  private
+
+  def render(message)
+    # message
+    foo = ApplicationController.renderer.render(
+      partial: 'messages/message',
+      locals: {
+        message: message
+      }
+    )
+    logger.info("FOO: #{foo}")
+    foo
+  end
+end
